27fd605366515a3afa1373a44007c161017d1875
Luis Reyes <alfredluis177@gmail.com>
2025-12-27 13:20:46 -0600

    Se añadieron los componentes vacios para dividir por componentes el crud.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Login } from "./components/intermediate/Login";
+import { UserCrud } from "./components/intermediate/UserCrud";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Login />
+    <UserCrud />
   </StrictMode>
 );
