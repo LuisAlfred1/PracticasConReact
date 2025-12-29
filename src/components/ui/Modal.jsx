83660a56c7fa1f2f24b11a8cc8601f1fceee06a6
Luis Reyes <alfredluis177@gmail.com>
2025-12-29 16:55:09 -0600

    El modal cierra al hacer click fuera (overlay)

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,9 +1,18 @@
-export const Modal = ({ isOpen, title, children }) => {
+//Añadimos onClose para cerrar el modal al hacer clic fuera de él.
+export const Modal = ({ isOpen, title, children, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/60 flex justify-center items-center z-50">
-      <div className="bg-slate-800 p-6 rounded-lg w-full max-w-sm">
+    <div
+      //Le pasamos el onClose al div de fondo para cerrar el modal al hacer clic fuera de él.
+      onClick={onClose}
+      className="fixed inset-0 bg-black/60 flex justify-center items-center z-50"
+    >
+      <div
+        //Este onClick evita que el clic dentro del modal cierre el modal.
+        onClick={(e) => e.stopPropagation()}
+        className="bg-slate-800 p-6 rounded-lg w-full max-w-sm"
+      >
         <h2 className="text-lg font-semibold mb-4">{title}</h2>
         {children}
       </div>
